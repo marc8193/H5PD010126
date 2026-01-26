f5ba77011df916eb96c4e4beb4b188eb9a217f0b
Marcus Haukelid Larsen <mhl@rtx.dk>
2026-01-26 13:07:05 +0100

    Implementation of Android app written only in C

---
+++ App2/hellofromc/app/main.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdio.h>
+
+extern struct android_app* gapp;
+
+void android_main(struct android_app* app) {
+    printf("Hello from %s!\n", APPNAME);
+    while (1) {
+        sleep(1);
+    }
+}
